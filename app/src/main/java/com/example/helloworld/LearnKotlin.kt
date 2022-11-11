4384ac44a71b9ebfce11431df36906efde464e65
mo <mo>
2022-11-11 18:15:02 +0800

    init

---
+++ app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+fun main() {
+    println("Hello Kotlin!")
+    val a = 10
+    println("a = " + a)
+    val b: Int = 20
+    println("b = " + b)
+
+    var c: Int = 10
+    c = c * 10
+    println("c = " + c)
+}
+
+class LearnKotlin {
+
+
+}
